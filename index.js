ae84b6a8241a712014ffd7dec3369ede8580a52f
behruzbek200909 <behruzbekmahkamov94@gmail.com>
2024-05-28 17:15:47 +0600

    Login

---
+++ index.js
@@ -0,0 +1,17 @@
+let inputValue = prompt("Ismingiz nima?")
+alert("Ismingiz kiritilgan..." + inputValue)
+
+let familiyangiz = prompt("Familiyangiz nima?")
+alert("Familiyangiz... kiritilgan?" + familiyangiz)
+
+let yoshingiz = prompt("Yoshingiz nechida?")
+alert("Yoshingiz... kiritilgan?" + yoshingiz)
+
+let qayerdansiz = prompt("Qayerdan siz?")
+alert("Qayerdanligiz...kiritilgan" + qayerdansiz)
+
+let yashashjoy = prompt("Yashash joyingizqayer?")
+alert("Yashash joyingiz... kiritilgan" + yashashjoy)
+
+
+
